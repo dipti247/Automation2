430c7acb1c2ee6168c34f20009c5202d01737509
QSP <QSP@DESKTOP-P1L9A95>
2018-09-28 13:56:14 +0530

    packages

--- src/script/TestDemo2.java
+++ src/script/TestDemo2.java
@@ -1,19 +1,22 @@
 package script;
 
+import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import generic.BaseTest;
 import generic.Utility;
 
-public class TestDemo extends BaseTest{
+public class TestDemo2 extends BaseTest{
  @Test
- public void testA() {
+ public void test2() {
 	String data = Utility.getXLData(INPUT_PATH, "sheet1",0,0);
 	Reporter.log("Data:"+data,true);
 	 
 	int r = Utility.getXLRowCount(INPUT_PATH, "sheet1");
 	Reporter.log("Row:"+r,true);
+	
+	Assert.fail();	
  }
 }
 
